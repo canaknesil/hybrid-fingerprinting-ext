978330b6304c3339b005f1c2ccc935d070b27979
Can Aknesil <aknesil@kth.se>
2024-05-17 15:36:18 +0200

    transfering a small dummy model to target's main memory

---
+++ capture/capture-ss-tflite.py
@@ -0,0 +1,152 @@
+import chipwhisperer as cw
+import time
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+import sys
+
+
+PLATFORM = "CWLITEXMEGA"
+fw_path = '../firmware/simpleserial-tflite/simpleserial-tflite-{}.hex'.format(PLATFORM)
+
+print("PLATFORM: ", PLATFORM)
+print("fw_path: ", fw_path)
+
+
+#
+# SETUP
+#
+
+# from Setup_Generic.ipynb
+scope = cw.scope()
+    
+try:
+    target = cw.target(scope)
+except IOError:
+    print("INFO: Caught exception on reconnecting to target - attempting to reconnect to scope first.")
+    print("INFO: This is a work-around when USB has died without Python knowing. Ignore errors above this line.")
+    scope = cw.scope()
+    target = cw.target(scope)
+
+print("INFO: Found ChipWhisperer😍")
+
+
+if "STM" in PLATFORM or PLATFORM == "CWLITEARM" or PLATFORM == "CWNANO":
+    prog = cw.programmers.STM32FProgrammer
+elif PLATFORM == "CW303" or PLATFORM == "CWLITEXMEGA":
+    prog = cw.programmers.XMEGAProgrammer
+else:
+    prog = None
+
+time.sleep(0.05)
+scope.default_setup()
+
+
+def reset_target(scope):
+    if PLATFORM == "CW303" or PLATFORM == "CWLITEXMEGA":
+        scope.io.pdic = 'low'
+        time.sleep(0.05)
+        scope.io.pdic = 'high_z' #XMEGA doesn't like pdic driven high
+        time.sleep(0.05)
+    else:  
+        scope.io.nrst = 'low'
+        time.sleep(0.05)
+        scope.io.nrst = 'high'
+        time.sleep(0.05)
+
+
+# from PA_CPA_1-Using_CW-Analyzer_for_CPA_Attack.ipynb
+cw.program_target(scope, prog, fw_path)
+time.sleep(1)
+project = cw.create_project("projects/ss-tflite", overwrite = True)
+
+
+#
+# UTILITY
+#
+
+def disconnect():
+    scope.dis()
+    target.dis()
+
+
+def ss_write(c, payload=[], timeout=2000):
+    #print("Sending command '{}'".format(c), end="")
+    target.simpleserial_write(c, payload)
+    
+    ret = target.simpleserial_wait_ack(timeout)
+    if ret is None:
+        raise Exception("Target failed to acknowledge!")
+
+    #print(" -> {}".format(ret))
+
+    
+def ss_read(c, payload_len, timeout=2000):
+    #print("Sending command '{}'".format(c), end="")
+    target.simpleserial_write(c, [])
+
+    payload = target.simpleserial_read('r', 64)
+    # target.simpleserial_read internally receives and checks ack
+
+    #print(" -> payload")
+
+    return payload
+
+
+#
+# SEND MODEL
+#
+
+model = bytearray([1, 2] * 32 + [3, 4] * 30)
+
+# Pad model with zeros until its length is multiple of 64.
+model += bytearray([0] * (-len(model) % 64))
+chunks = [model[i:i+64] for i in range(0, len(model), 64)]
+
+print("Sending the model.")
+ss_write('a')
+for chunk in tqdm(chunks):
+    ss_write('b', chunk)
+
+
+#
+# VERIFY MODEL
+#
+
+print("Reading the model back for verification.")
+chunks2 = []
+ss_write('c')
+for chunk in tqdm(chunks):
+    chunk2 = ss_read('d', 64)
+    if chunk != chunk2:
+        raise Exception("Readback model does not match the original!")
+    
+print("Verification successful.")
+
+
+#
+# CAPTURE
+#
+
+# ktp = cw.ktp.Basic()
+
+# num_traces = 2
+
+# print("Capturing traces...")
+# for i in tqdm(range(num_traces)):
+#     key, text = ktp.next()  # manual creation of a key, text pair can be substituted here
+#     trace = cw.capture_trace(scope, target, text, key)
+#     if trace is None:
+#         continue
+#     project.traces.append(trace)
+
+# project.save()
+# plt.plot(project.waves[0])
+
+#
+# DISCONNECT
+#
+
+disconnect()
+
+
+#plt.show()
--- firmware/simpleserial-tflite/simpleserial-tflite.cpp
+++ firmware/simpleserial-tflite/simpleserial-tflite.cpp
@@ -1,147 +1,123 @@
-/*
-This file is part of the ChipWhisperer Example Targets
-Copyright (C) 2012-2017 NewAE Technology Inc.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
 extern "C" {
-#include "aes-independant.h"
 #include "hal.h"
 #include "simpleserial.h"
 #include <stdint.h>
 #include <stdlib.h>
 }
 
-uint8_t get_mask(uint8_t* m, uint8_t len)
+
+// uint8_t get_mask(uint8_t* m, uint8_t len)
+// {
+//    aes_indep_mask(m, len);
+//    return 0x00;
+// }
+
+// uint8_t get_key(uint8_t* k, uint8_t len)
+// {
+//    aes_indep_key(k);
+//    return 0x00;
+// }
+
+// uint8_t get_pt(uint8_t* pt, uint8_t len)
+// {
+//    aes_indep_enc_pretrigger(pt);
+
+//    trigger_high();
+
+// #ifdef ADD_JITTER
+//    for (volatile uint8_t k = 0; k < (*pt & 0x0F); k++);
+// #endif
+
+//    aes_indep_enc(pt); /* encrypting the data block */
+//    trigger_low();
+
+//    aes_indep_enc_posttrigger(pt);
+
+//    simpleserial_put('r', 16, pt);
+//    return 0x00;
+// }
+
+// uint8_t reset(uint8_t* x, uint8_t len)
+// {
+//    // Reset key here if needed
+//    return 0x00;
+// }
+
+// static uint16_t num_encryption_rounds = 10;
+
+// uint8_t enc_multi_getpt(uint8_t* pt, uint8_t len)
+// {
+//    aes_indep_enc_pretrigger(pt);
+
+//    for(unsigned int i = 0; i < num_encryption_rounds; i++){
+//       trigger_high();
+//       aes_indep_enc(pt);
+//       trigger_low();
+//    }
+
+//    aes_indep_enc_posttrigger(pt);
+//    simpleserial_put('r', 16, pt);
+//    return 0;
+// }
+
+// uint8_t enc_multi_setnum(uint8_t* t, uint8_t len)
+// {
+//    //Assumes user entered a number like [0, 200] to mean "200"
+//    //which is most sane looking for humans I think
+//    num_encryption_rounds = t[1];
+//    num_encryption_rounds |= t[0] << 8;
+//    return 0;
+// }
+
+
+// Simpleserial supports receiving at most 64 bytes at a
+// time. Receiving the models in 64 byte chunks.
+
+// For now using the main memory for the model for testing.
+static uint8_t model[128];
+
+static size_t model_write_addr_offset = 0;
+static size_t model_read_addr_offset = 0;
+
+static uint8_t get_model_reset(uint8_t* data, uint8_t len)
 {
-   aes_indep_mask(m, len);
+   model_write_addr_offset = 0;
    return 0x00;
 }
 
-uint8_t get_key(uint8_t* k, uint8_t len)
+static uint8_t get_model_64(uint8_t* data, uint8_t len)
 {
-   aes_indep_key(k);
+   for (size_t i=0; i<64; i++)
+      model[model_write_addr_offset++] = data[i];
+   
    return 0x00;
 }
 
-uint8_t get_pt(uint8_t* pt, uint8_t len)
+static uint8_t put_model_reset(uint8_t* data, uint8_t len)
 {
-   aes_indep_enc_pretrigger(pt);
-
-   trigger_high();
-
-#ifdef ADD_JITTER
-   for (volatile uint8_t k = 0; k < (*pt & 0x0F); k++);
-#endif
-
-   aes_indep_enc(pt); /* encrypting the data block */
-   trigger_low();
-
-   aes_indep_enc_posttrigger(pt);
-
-   simpleserial_put('r', 16, pt);
+   model_read_addr_offset = 0;
    return 0x00;
 }
 
-uint8_t reset(uint8_t* x, uint8_t len)
+static uint8_t put_model_64(uint8_t* data, uint8_t len)
 {
-   // Reset key here if needed
+   simpleserial_put('r', 64, model + model_read_addr_offset);
+   model_read_addr_offset += 64;
+   
    return 0x00;
 }
 
-static uint16_t num_encryption_rounds = 10;
-
-uint8_t enc_multi_getpt(uint8_t* pt, uint8_t len)
-{
-   aes_indep_enc_pretrigger(pt);
-
-   for(unsigned int i = 0; i < num_encryption_rounds; i++){
-      trigger_high();
-      aes_indep_enc(pt);
-      trigger_low();
-   }
-
-   aes_indep_enc_posttrigger(pt);
-   simpleserial_put('r', 16, pt);
-   return 0;
-}
-
-uint8_t enc_multi_setnum(uint8_t* t, uint8_t len)
-{
-   //Assumes user entered a number like [0, 200] to mean "200"
-   //which is most sane looking for humans I think
-   num_encryption_rounds = t[1];
-   num_encryption_rounds |= t[0] << 8;
-   return 0;
-}
-
-#if SS_VER == SS_VER_2_1
-uint8_t aes(uint8_t cmd, uint8_t scmd, uint8_t len, uint8_t *buf)
-{
-   uint8_t req_len = 0;
-   uint8_t err = 0;
-   uint8_t mask_len = 0;
-   if (scmd & 0x04) {
-      // Mask has variable length. First byte encodes the length
-      mask_len = buf[req_len];
-      req_len += 1 + mask_len;
-      if (req_len > len) {
-	 return SS_ERR_LEN;
-      }
-      err = get_mask(buf + req_len - mask_len, mask_len);
-      if (err)
-	 return err;
-   }
-
-   if (scmd & 0x02) {
-      req_len += 16;
-      if (req_len > len) {
-	 return SS_ERR_LEN;
-      }
-      err = get_key(buf + req_len - 16, 16);
-      if (err)
-	 return err;
-   }
-   if (scmd & 0x01) {
-      req_len += 16;
-      if (req_len > len) {
-	 return SS_ERR_LEN;
-      }
-      err = get_pt(buf + req_len - 16, 16);
-      if (err)
-	 return err;
-   }
-
-   if (len != req_len) {
-      return SS_ERR_LEN;
-   }
-
-   return 0x00;
-
-}
-#endif
 
 int main(void)
 {
-   uint8_t tmp[KEY_LENGTH] = {DEFAULT_KEY};
+   // uint8_t tmp[KEY_LENGTH] = {DEFAULT_KEY};
 
    platform_init();
    init_uart();
    trigger_setup();
 
-   aes_indep_init();
-   aes_indep_key(tmp);
+   // aes_indep_init();
+   // aes_indep_key(tmp);
 
    /* Uncomment this to get a HELLO message for debug */
 
@@ -153,16 +129,19 @@ int main(void)
    // putch('\n');
 
    simpleserial_init();
-#if SS_VER == SS_VER_2_1
-   simpleserial_addcmd(0x01, 16, aes);
-#else
-   simpleserial_addcmd('k', 16, get_key);
-   simpleserial_addcmd('p', 16,  get_pt);
-   simpleserial_addcmd('x',  0,   reset);
-   simpleserial_addcmd_flags('m', 18, get_mask, CMD_FLAG_LEN);
-   simpleserial_addcmd('s', 2, enc_multi_setnum);
-   simpleserial_addcmd('f', 16, enc_multi_getpt);
-#endif
+
+   // simpleserial_addcmd('k', 16, get_key);
+   // simpleserial_addcmd('p', 16,  get_pt);
+   // simpleserial_addcmd('x',  0,   reset);
+   // simpleserial_addcmd_flags('m', 18, get_mask, CMD_FLAG_LEN);
+   // simpleserial_addcmd('s', 2, enc_multi_setnum);
+   // simpleserial_addcmd('f', 16, enc_multi_getpt);
+
+   simpleserial_addcmd('a', 0, get_model_reset);
+   simpleserial_addcmd('b', 64, get_model_64);
+   simpleserial_addcmd('c', 0, put_model_reset);
+   simpleserial_addcmd('d', 0, put_model_64);
+
    while(1)
       simpleserial_get();
 }
