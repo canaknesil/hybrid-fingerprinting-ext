4dcd5cf909ac67e39ebc973d76656a62f02d3271
Can Aknesil <aknesil@kth.se>
2024-07-08 22:13:06 +0200

    analysis with traces

--- sca/detect-stolen-model.py
+++ sca/detect-stolen-model.py
@@ -1,6 +1,14 @@
 import sys
 import numpy as np
+import scipy.stats as st
+import scipy.signal as sig
+import matplotlib.pyplot as plt
+from tqdm import tqdm
 
+
+#
+# PROCESS COMMAND LINE INPUTS
+#
 
 original_prefix = sys.argv[1]
 suspect_prefix = sys.argv[2]
@@ -50,8 +58,240 @@ def logits_to_predictions(logits):
 logits_list = outputs_list
 outputs_list = list(map(logits_to_predictions, outputs_list))
 
+outputs_original, outputs_suspect, outputs_third, correct_outputs = outputs_list
+logits_original, logits_suspect, logits_third, logits_correct = logits_list
+
+
+traces_original = np.load(original_prefix + "_traces.npy")
+traces_suspect = np.load(suspect_prefix + "_traces.npy")
+traces_third = np.load(third_prefix + "_traces.npy")
+
+print("traces_original.shape:", traces_original.shape)
+print("traces_suspect.shape:", traces_suspect.shape)
+print("traces_third.shape:", traces_third.shape)
+
+# Multi-scale representation
+dtraces_original = [traces_original]
+dtraces_suspect = [traces_suspect]
+dtraces_third = [traces_third]
+
+for i in range(4):
+    dtraces_original.append(sig.decimate(dtraces_original[-1], 2))
+    dtraces_suspect.append(sig.decimate(dtraces_suspect[-1], 2))
+    dtraces_third.append(sig.decimate(dtraces_third[-1], 2))
+
+
+#
+# UTILS
+#
+
+# Result is in range [0, 2]
+def logit_distance(v1, v2):
+    return np.sum(np.abs(v1 - v2))
+
+
+def logits_distance(a, b):
+    dist = np.zeros(a.shape[0])
+    for i in range(a.shape[0]):
+        dist[i] = logit_distance(a[i], b[i])
+    return dist
+
+
+def print_histogram(v, *args, **kwargs):
+    hist, bin_edges = np.histogram(v, *args, **kwargs)
+    print(f"Range:", (bin_edges[0], bin_edges[-1]))
+    print("Histogram:", hist)
+
+
+def one_sample_ttest(a, mean_b):
+    return (np.average(a) - mean_b) / (np.std(a) / np.sqrt(len(a)))
+
+
+def ttest(a, b):
+    return st.ttest_ind(a, b).statistic
+
+
+def pdf(samples, x=None):
+    if x is None:
+        x = np.linspace(samples.min(), samples.max(), 100)
+        
+    kde = st.gaussian_kde(samples)
+    return x, kde(x)
+
+
+def pdf_overlap_area(pdf_a, pdf_b, dx=1):
+    return np.sum(np.minimum(pdf_a, pdf_b)) * dx
+
+
+def overlap_between_traces(traces_a, traces_b):
+    overlap = np.zeros(traces_a.shape[-1])
+    for i in tqdm(range(traces_a.shape[-1])):
+        a = traces_a[:,i]
+        b = traces_b[:,i]
+        min_x = min(list(map(np.min, [a, b])))
+        max_x = max(list(map(np.max, [a, b])))
+        x = np.linspace(min_x, max_x, 50)
+        dx = x[1] - x[0]
+        _, pdf_a = pdf(a, x)
+        _, pdf_b = pdf(b, x)
+        overlap[i] = pdf_overlap_area(pdf_a, pdf_b, dx=dx)
+    return overlap
+
+
+#
+# MAIN
+#
+
+print("\nAnalyzing class predictions.")
+
+# indices where the original and the 3rd party model have different class predictions
+diff_original_vs_third_idx = np.nonzero(outputs_original != outputs_third)[0]
+print(f"{len(diff_original_vs_third_idx)} out of {len(outputs_original)} ({len(diff_original_vs_third_idx) / len(outputs_original) * 100}%) class predictions of the original model differs from the 3rd-party model.")
+
+diff_original_vs_suspect_idx = np.nonzero(outputs_original[diff_original_vs_third_idx] != outputs_suspect[diff_original_vs_third_idx])[0]
+print(f"For the inputs that produce different prediction by the original and the 3rd-party models, {len(diff_original_vs_suspect_idx)} out of {len(diff_original_vs_third_idx)} ({len(diff_original_vs_suspect_idx) / len(diff_original_vs_third_idx) * 100}%) class predictions of the original model differs from the suspect model.")
+
+
+print("\nAnalyzing logits.")
+
+diff_original_vs_third = logits_distance(logits_original, logits_third)
+print("Histogram of logit distance between the original and 3rd-party models:")
+print_histogram(diff_original_vs_third, range=(0, 2))
+
+diff_original_vs_suspect = logits_distance(logits_original, logits_suspect)
+print("Histogram of logit distance between the original and suspect models:")
+print_histogram(diff_original_vs_suspect, range=(0, 2))
+
+logit_dist_threshold = 1
+diff_original_vs_third_idx = np.nonzero(diff_original_vs_third > logit_dist_threshold)[0]
+print(f"{len(diff_original_vs_third_idx)} out of {len(outputs_original)} ({len(diff_original_vs_third_idx) / len(outputs_original) * 100}%) logits of the original model has at least {logit_dist_threshold} distant from the 3rd-party model.")
+
+diff_original_vs_suspect = logits_distance(logits_original[diff_original_vs_third_idx], logits_suspect[diff_original_vs_third_idx])
+print("For these inputs, logit distance between the original and suspect models:")
+print_histogram(diff_original_vs_suspect, range=(0, 2))
+
+
+print("\nAnalyzing traces.")
+
+scale = 3
+
+print("Calculating overlap between original and 3rd-party models:")
+overlap_original_vs_third = overlap_between_traces(dtraces_original[scale], dtraces_third[scale])
+print("Calculating overlap between original and suspect models:")
+overlap_original_vs_suspect = overlap_between_traces(dtraces_original[scale], dtraces_suspect[scale])
+
+plt.figure()
+plt.plot(overlap_original_vs_third)
+plt.plot(overlap_original_vs_suspect)
+plt.ylim(-1, 1)
     
-# TODO: Consider traces
+plt.figure()
+plt.plot(overlap_original_vs_suspect - overlap_original_vs_third)
+plt.ylim(-1, 1)
+plt.title("PDF overlap increase from 3rd-party to suspect")
+
+print("Overlap area original vs 3rd-party:")
+print_histogram(overlap_original_vs_third, range=(0, 1))
+print("Overlap area original vs suspect:")
+print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+print("Average overlap area between original vs. 3rd-party:", np.average(overlap_original_vs_third))
+print("Average overlap area between original vs. suspect  :", np.average(overlap_original_vs_suspect))
+
+
+# Find indices with less overlap between original vs. 3rd-party
+overlap_threshold = 1 # Distance from mean where two Gaussian distribution cross (in terms of standard deviation)
+overlap_area_threshold = st.norm.cdf(-overlap_threshold) * 2 # in range [0, 1]
+print("overlap_area_threshold:", overlap_area_threshold)
+
+significant_trace_points = np.nonzero(overlap_original_vs_third < overlap_area_threshold)[0]
+print(f"{len(significant_trace_points)} trace points are significant.")
+print_histogram(significant_trace_points)
+
+
+# Find indices of inputs that produce less overlap between the original and 3rd-party model.
+diff_original_vs_third = np.sum(np.abs(dtraces_original[scale] - dtraces_third[scale]), axis=1)
+print("Sum of absolute differences between original and 3rd-party models across trace points:")
+print_histogram(diff_original_vs_third)
+
+trace_diff_threshold = (diff_original_vs_third.max() + diff_original_vs_third.min()) / 2
+print("trace_diff_threshold:", trace_diff_threshold)
+diff_original_vs_third_idx = np.nonzero(diff_original_vs_third > trace_diff_threshold)[0]
+
+
+# Calculate overlap between the original model and the suspect using only significant points.
+print("\nUsing only significant indices.")
+print("Calculating overlap between original and 3rd-party models:")
+a = dtraces_original[scale][diff_original_vs_third_idx][:,significant_trace_points]
+b = dtraces_suspect[scale][diff_original_vs_third_idx][:,significant_trace_points]
+c = dtraces_third[scale][diff_original_vs_third_idx][:,significant_trace_points]
+
+overlap_original_vs_third = overlap_between_traces(a, c)
+print("Calculating overlap between original and suspect models:")
+overlap_original_vs_suspect = overlap_between_traces(a, b)
+
+print("Overlap area original vs 3rd-party:")
+print_histogram(overlap_original_vs_third, range=(0, 1))
+print("Overlap area original vs suspect:")
+print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+print("Average overlap area between original vs. 3rd-party:", np.average(overlap_original_vs_third))
+print("Average overlap area between original vs. suspect  :", np.average(overlap_original_vs_suspect))
+
+
+#plt.show()
+sys.exit()
+
+
+
+overlap_original_vs_third = np.zeros(dtraces_original[scale].shape[-1])
+overlap_original_vs_suspect = np.zeros(dtraces_original[scale].shape[-1])
+
+print("Calculating PDF overlaps for all trace points:")
+for i in tqdm(range(dtraces_original[scale].shape[-1])):
+#for i in tqdm(range(2000)): # fast execution for debugging
+    a = dtraces_original[scale][:,i]
+    b = dtraces_suspect[scale][:,i]
+    c = dtraces_third[scale][:,i]
+    min_x = min(list(map(np.min, [a, b, c])))
+    max_x = max(list(map(np.max, [a, b, c])))
+    x = np.linspace(min_x, max_x, 50)
+    dx = x[1] - x[0]
+    _, pdf_a = pdf(a, x)
+    _, pdf_b = pdf(b, x)
+    _, pdf_c = pdf(c, x)
+
+    overlap_original_vs_third[i] = pdf_overlap_area(pdf_a, pdf_c, dx=dx)
+    overlap_original_vs_suspect[i] = pdf_overlap_area(pdf_a, pdf_b, dx=dx)
+
+
+plt.figure()
+plt.plot(overlap_original_vs_third)
+plt.plot(overlap_original_vs_suspect)
+plt.ylim(-1, 1)
+    
+plt.figure()
+plt.plot(overlap_original_vs_suspect - overlap_original_vs_third)
+plt.ylim(-1, 1)
+plt.title("PDF overlap increase from 3rd-party to suspect")
+
+    
+#overlap_threshold = 1 # Distance from mean where two Gaussian distribution cross (in terms of standard deviation)
+#overlap_area_threshold = st.norm.cdf(-overlap_threshold) * 2 # in range [0, 1]
+#print("overlap_area_threshold:", overlap_area_threshold)
+
+print("Overlap area original vs 3rd-party:")
+print_histogram(overlap_original_vs_third, range=(0, 1))
+print("Overlap area original vs suspect:")
+print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+print("Average overlap area between original vs. 3rd-party:", np.average(overlap_original_vs_third))
+print("Average overlap area between original vs. suspect  :", np.average(overlap_original_vs_suspect))
+
+
+
+plt.show()
+sys.exit()
 
 
 def isequal(a1, a2):
@@ -82,8 +322,7 @@ def detect_direct_copy(outputs_original, outputs_suspect, outputs_third, correct
     print(f"{n_similar_third} out of {size} ({similarity_rate_third*100}%) predictions by the 3rd-party model are similar to the original model.")
 
 
-def logit_distance(v1, v2):
-    return np.sqrt(np.sum(np.square(v1 - v2)))
+
 
 
 def avg_logit_distance(a, b):
