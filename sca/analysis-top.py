40ef8d39bfc22b2f131645a15bf46f085ee07d99
Can Aknesil <aknesil@kth.se>
2024-07-15 18:29:19 +0200

    top analysis for every important pair

--- sca/analysis-top.py
+++ sca/analysis-top.py
@@ -1,12 +1,18 @@
 import compare_models
+from compare_models import compare_model_outputs, compare_traces, decimate_traces
 from glob import glob
 import os
 import re
 import sys
+import numpy as np
+from tqdm import tqdm
 
+
+#
+# PREPARE MODELS TO BE COMPARED
+#
 
 workspace = "workspace"
-
 
 # With help from ChatGPT
 def natural_sort_key(s):
@@ -37,6 +43,11 @@ print_models("models_snr_100", models_snr_100)
 print_models("models_retrained", models_retrained)
 
 print()
+correct_outputs = workspace + "/mnist_7x7_indep-0_y_test.npy"
+print("Correct outputs:", correct_outputs)
+correct_outputs = np.load(correct_outputs)
+
+print()
 pairs_snr_100 = list(zip(models, models_snr_100))
 print_models("Original vs. Noisy pairs", pairs_snr_100)
 
@@ -54,12 +65,160 @@ pairs_third = list(zip([models[0]] * (n-2), models[2:]))
 print_models("Original vs. 3rd-party pairs", pairs_third)
 
 
-# TODO: Load correct output file
-# TODO: Information tuple output from compare_model
-# TODO: Pandas table views of the collected and processed info.
-# TODO: Try different overlap area methods.
+#
+# METRICS TO EVALUATE
+#
+
+# Inputs that is provided to the model during inference. "regular"
+# inputs are similar to the ones the models is trained with. "random"
+# inputs are generated randomly and doesn't have a corresponding
+# label.
+query_types = ["regular", "random"]
+
+# TODO: add "copy" type
+extraction_methods = ["snr-100", "retrained", "third"]
+
+# Information whose similarity between the original and the suspect model will be analyzed.
+metric_types = ["class_prediction", "logit", "trace_overlap"]
+
+# The method used to calculate overlap between probability
+# distributions between traces from the original and the suspect
+# model. Trying only kde and histogram. Numerical integral method
+# "integral" is very slow, "gaussian" is wrong.
+#overlap_methods = ["kde", "histogram"]
+overlap_methods = ["histogram"]
+
+# The filtering that will be applied to the above
+# information. "when_orig_wrong" compares only the cases where the
+# original model's predictions are wrong.
+output_filters = ["none", "when_orig_wrong"]
+
+
+# TODO: Incorporate overlap_methods into metric_types as e.g. "trace_overlap_kde"
+# TODO: Incorporate decimation
+
+
+#
+# COMPARE MODELS
+#
+
+def init_results(dims):
+    if len(dims) == 0:
+        return []
+    else:
+        res = {}
+        for x in dims[0]:
+            res[x] = init_results(dims[1:])
+        return res
+
+
+def load_data(prefix):
+    outputs = np.load(prefix + "_outputs.npy")
+    traces = np.load(prefix + "_traces.npy")
+
+    traces = decimate_traces(traces, 2, 3)
+    
+    return outputs, traces
+
+
+
+results = init_results([query_types, extraction_methods, metric_types, output_filters])
+model_pairs_dict = dict(zip(extraction_methods, [pairs_snr_100, pairs_retrained, pairs_third]))
+
+for extraction_method in extraction_methods:
+    
+    for query_type in query_types:
+        print("\nExtraction method:", extraction_method)
+        print("Query type:", query_type)
+    
+        model_pairs = model_pairs_dict[extraction_method]
+
+        # Load outputs and traces
+        print("Loading data.")
+        models = []
+        for p in model_pairs:
+            models += p
+        models = set(models)
+
+        data = {}
+        for m in models:
+            prefix = workspace + "/" + m
+            if query_type == "random":
+                prefix += "_rand-x"
+            print(f"  {prefix}")
+            data[m] = load_data(prefix)
+
+        print("Comparing models:")
+        for m1, m2 in model_pairs:
+            print(f"  ({m1}, {m2})")
+            m1_outputs, m1_traces = data[m1]
+            m2_outputs, m2_traces = data[m2]
+
+            if query_type == "random":
+                m1 += "_rand-x"
+                corr_outputs = None
+            else:
+                corr_outputs = correct_outputs
+                
+            res = compare_model_outputs(m1_outputs, m2_outputs, corr_outputs)
+            results[query_type][extraction_method]["class_prediction"]["none"].append(res["class_predictions"])
+            results[query_type][extraction_method]["logit"]["none"].append(res["logits"])
+
+            if corr_outputs is not None:
+                results[query_type][extraction_method]["class_prediction"]["when_orig_wrong"].append(res["class_predictions_when_orig_wrong"])
+                results[query_type][extraction_method]["logit"]["when_orig_wrong"].append(res["logits_when_orig_wrong"])
+
+            res = compare_traces(m1_outputs, m2_outputs, corr_outputs, m1_traces, m2_traces, overlap_method="histogram")
+            results[query_type][extraction_method]["trace_overlap"]["none"].append(res["trace_overlap"])
+
+            if corr_outputs is not None:
+                results[query_type][extraction_method]["trace_overlap"]["when_orig_wrong"].append(res["trace_overlap_when_orig_wrong"])
+
+
+
+            
+#
+# ANALYIS OF RESULTS
+#
+
+def result_to_str(r):
+    r = np.array(r)
+    assert(len(r.shape) == 1)
+    
+    if len(r) == 0:
+        mean = float("nan")
+        std = float("nan")
+        size = 0
+    elif len(r) == 1:
+        mean = r[0]
+        std = float("nan")
+        size = 1
+    else:
+        mean = np.average(r)
+        std = np.std(r)
+        size = len(r)
+        
+    return f"{mean:.5f} +- {std:.5f} (out of {size})"
+
+
+
+for a in query_types:
+    for c in metric_types:
+        for d in output_filters:
+            print()
+            print(f"query_type: {a}, metric_type: {c}, output_filter: {d}")
+            orig_vs_snr = results[a]["snr-100"][c][d]
+            orig_vs_retrained = results[a]["retrained"][c][d]
+            orig_vs_third = results[a]["third"][c][d]
+
+            print("Original vs. Noisy    :", result_to_str(orig_vs_snr))
+            print("Original vs. Retrained:", result_to_str(orig_vs_retrained))
+            print("Original vs. 3rd-party:", result_to_str(orig_vs_third))
+                  
+        
+
+
 # TODO: For copy model, split the traces in 2 and compare to each other.
 
 
-#compare_models(original_prefix, suspect_prefix, correct_outputs_file)
 
--- sca/compare_models.py
+++ sca/compare_models.py
@@ -104,7 +104,7 @@ def pdf_overlap_area_hist(samples_a, samples_b):
     min_s = min(np.min(samples_a), np.min(samples_b))
     max_s = max(np.max(samples_a), np.max(samples_b))
 
-    n_bins = 30 # This number influences the absolute result significantly.
+    n_bins = 20 # This number influences the absolute result significantly.
     bins = np.linspace(min_s, max_s, n_bins)
     hist_a, _ = np.histogram(samples_a, bins=bins, density=True)
     hist_b, _ = np.histogram(samples_b, bins=bins, density=True)
@@ -135,74 +135,156 @@ def pdf_overlap_area_gaussian(samples_a, samples_b):
     
 
 
-def overlap_between_traces(traces_a, traces_b):
+def overlap_between_traces(traces_a, traces_b, method="kde", verbose=False):
     overlap = np.zeros(traces_a.shape[-1])
-    for i in tqdm(range(traces_a.shape[-1])):
+
+    v_range = range(traces_a.shape[-1])
+
+    if verbose:
+        v_range = tqdm(v_range)
+    
+    for i in v_range:
         a = traces_a[:,i]
         b = traces_b[:,i]
 
-        # Method with KDE (slow)
-        overlap[i] = pdf_overlap_area(a, b)
-
-        # Method with numerical integration (very slow)
-        #overlap[i] = pdf_overlap_area_quad(a, b)
-
-        # Method with histogram (very fast, but not accurate, parametric)
-        #overlap[i] = pdf_overlap_area_hist(a, b)
-
-        # Method with Bhattacharyya coefficient (very fast, assuming
-        # gaussian dists) This method returns very high overlap every
-        # almost every time. Maybe distributions are not close to
-        # Gaussian.
-        #overlap[i] = pdf_overlap_area_gaussian(a, b)
+        if method == "kde":
+            # Method with KDE (slow)
+            overlap[i] = pdf_overlap_area(a, b)
+        elif method == "integral":
+            # Method with numerical integration (very slow)
+            overlap[i] = pdf_overlap_area_quad(a, b)
+        elif method == "histogram":
+            # Method with histogram (very fast, but not accurate, parametric)
+            overlap[i] = pdf_overlap_area_hist(a, b)
+        elif method == "gaussian":
+            # Method with Bhattacharyya coefficient (very fast,
+            # assuming gaussian dists) This method returns very high
+            # overlap every almost every time. Maybe distributions are
+            # not close to Gaussian.
+            overlap[i] = pdf_overlap_area_gaussian(a, b)
+        else:
+            raise Exception(f"Unsupported overlap area calculation method: {method}")            
         
     return overlap
+
+
+def read_model_outputs(outputs_original, outputs_suspect, correct_outputs=None, verbose=False):
+    if verbose:
+        print("outputs_original.shape:", outputs_original.shape)
+        print("outputs_suspect.shape:", outputs_suspect.shape)
+        if correct_outputs is not None:
+            print("correct_outputs.shape:", correct_outputs.shape)
+
+    if verbose:
+        print("Removing dimensions of size 1.")
+        
+    outputs_original = np.squeeze(outputs_original)
+    outputs_suspect = np.squeeze(outputs_suspect)
+    if correct_outputs is not None:
+        correct_outputs = np.squeeze(correct_outputs)
+    
+    if correct_outputs is not None:
+        assert outputs_original.shape[0] <= correct_outputs.shape[0]
+        if outputs_original.shape[0] < correct_outputs.shape[0]:
+            if verbose:
+                print("Warning: Traces were collected from a subset of test data. Choping test data.")
+            correct_outputs = correct_outputs[:outputs_original.shape[0],:]
+
+    assert outputs_suspect.shape == outputs_original.shape
+    if correct_outputs is not None:
+        assert correct_outputs.shape == outputs_original.shape
+
+    if correct_outputs is not None:
+        return outputs_original, outputs_suspect, correct_outputs
+    else:
+        return outputs_original, outputs_suspect, None
+
 
 
 #
 # Interface
 #
 
-def compare_models(original_prefix, suspect_prefix, correct_outputs_file, decimation_factor=2, n_decimation=3):
-
+def compare_model_outputs(outputs_original, outputs_suspect, correct_outputs=None, verbose=False):
     # PROCESS COMMAND LINE INPUTS
 
-    outputs_original = np.load(original_prefix + "_outputs.npy")
-    outputs_suspect = np.load(suspect_prefix + "_outputs.npy")
-    correct_outputs = np.load(correct_outputs_file)
+    # Model predictions (outputs)
+    outputs_original, outputs_suspect, correct_outputs = read_model_outputs(outputs_original, outputs_suspect, correct_outputs, verbose=verbose)
     
-    print("outputs_original.shape:", outputs_original.shape)
-    print("outputs_suspect.shape:", outputs_suspect.shape)
-    print("correct_outputs.shape:", correct_outputs.shape)
+    logits_original = outputs_original
+    logits_suspect = outputs_suspect
+    if correct_outputs is not None:
+        logits_correct = correct_outputs
+
+    outputs_original = logits_to_predictions(outputs_original)
+    outputs_suspect = logits_to_predictions(outputs_suspect)
+    if correct_outputs is not None:
+        correct_outputs = logits_to_predictions(correct_outputs)
+
+        
+    # ANALYSIS
+
+    results = {}
+
+    if verbose:
+        print("\nAnalyzing class predictions.")
     
-    print("Removing dimensions of size 1.")
-    outputs_original = np.squeeze(outputs_original)
-    outputs_suspect = np.squeeze(outputs_suspect)
-    correct_outputs = np.squeeze(correct_outputs)
+    diff_original_vs_suspect_idx = np.nonzero(outputs_original != outputs_suspect)[0]
+    results["class_predictions"] = 1 - len(diff_original_vs_suspect_idx) / len(outputs_original)
+
+    if verbose:
+        print("Class predictions that differ for the original and the suspect model:")
+        print(f"  {frac_str(len(diff_original_vs_suspect_idx), len(outputs_original))}")
+
+    if correct_outputs is not None:
+        diff_original_vs_correct_idx = np.nonzero(outputs_original != correct_outputs)[0]
+        diff_original_vs_suspect_idx = np.nonzero(outputs_original[diff_original_vs_correct_idx] != outputs_suspect[diff_original_vs_correct_idx])[0]
+        results["class_predictions_when_orig_wrong"] = 1 - len(diff_original_vs_suspect_idx) / len(diff_original_vs_correct_idx)
+
+        if verbose:
+            print("For the cases where the original model's predictions are not correct, class predictions that differ for the original and the suspect model:")
+            print(f"  {frac_str(len(diff_original_vs_suspect_idx), len(diff_original_vs_correct_idx))}")
     
+
+    if verbose:
+        print("\nAnalyzing logits.")
     
-    assert outputs_original.shape[0] <= correct_outputs.shape[0]
-    if outputs_original.shape[0] < correct_outputs.shape[0]:
-        print("Warning: Traces were collected from a subset of test data. Choping test data.")
-        correct_outputs = correct_outputs[:outputs_original.shape[0],:]
+    diff_original_vs_suspect = logits_distance(logits_original, logits_suspect)
+    results["logits"] = 2 - np.average(diff_original_vs_suspect)
+
+    if verbose:
+        print("Logit distances between the original and suspect model:")
+        print_histogram(diff_original_vs_suspect, range=(0, 2))
+
+    if correct_outputs is not None:
+        diff_original_vs_suspect = logits_distance(logits_original[diff_original_vs_correct_idx], logits_suspect[diff_original_vs_correct_idx])
+        results["logits_when_orig_wrong"] = 2 - np.average(diff_original_vs_suspect)
+
+        if verbose:
+            print("For the cases where the original model's predictions are not correct, logit distances between the original and suspect model:")
+            print_histogram(diff_original_vs_suspect, range=(0, 2))
     
-    outputs_list = [outputs_original, outputs_suspect, correct_outputs]
-    for x in outputs_list[1:]:
-        assert x.shape == outputs_list[0].shape
+    return results
+
+
+
+
+def compare_traces(outputs_original, outputs_suspect, correct_outputs, traces_original, traces_suspect, overlap_method="kde", verbose=False, plot=False):
+    # PROCESS COMMAND LINE INPUTS
+
+    # Model predictions (outputs)
+    outputs_original, outputs_suspect, correct_outputs = read_model_outputs(outputs_original, outputs_suspect, correct_outputs)
     
-    
-    logits_list = outputs_list
-    outputs_list = list(map(logits_to_predictions, outputs_list))
-    
-    outputs_original, outputs_suspect, correct_outputs = outputs_list
-    logits_original, logits_suspect, logits_correct = logits_list
-    
-    
-    traces_original = np.load(original_prefix + "_traces.npy")
-    traces_suspect = np.load(suspect_prefix + "_traces.npy")
-    
-    print("traces_original.shape:", traces_original.shape)
-    print("traces_suspect.shape:", traces_suspect.shape)
+    outputs_original = logits_to_predictions(outputs_original)
+    outputs_suspect = logits_to_predictions(outputs_suspect)
+    if correct_outputs is not None:
+        correct_outputs = logits_to_predictions(correct_outputs)
+
+        
+    # Traces
+    if verbose:
+        print("traces_original.shape:", traces_original.shape)
+        print("traces_suspect.shape:", traces_suspect.shape)
     
     #points_start = 0
     #points_stop = 3200
@@ -226,78 +308,79 @@ def compare_models(original_prefix, suspect_prefix, correct_outputs_file, decima
     #     dtraces_suspect.append(sig.decimate(dtraces_suspect[-1], 2))
     #     dtraces_third.append(sig.decimate(dtraces_third[-1], 2))
     
-    print("decimation:", decimation_factor ** n_decimation)
-    
-    for i in range(n_decimation):
-        traces_original = sig.decimate(traces_original, 2)
-        traces_suspect = sig.decimate(traces_suspect, 2)
-    
     
     # ANALYSIS
+
+    results = {}
     
-    print("\nAnalyzing class predictions.")
+    if correct_outputs is not None:
+        diff_original_vs_correct_idx = np.nonzero(outputs_original != correct_outputs)[0]
+
+    if verbose:
+        print("\nAnalyzing traces.")
+
+    if verbose:
+        print("Calculating overlap between original and suspect model:")
+    overlap_original_vs_suspect = overlap_between_traces(traces_original, traces_suspect, method=overlap_method, verbose=verbose)
+    results["trace_overlap"] = np.average(overlap_original_vs_suspect)
+
+    if verbose:
+        print("Overlap area original vs suspect:")
+        print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+    if plot:
+        plt.figure()
+        plt.plot(overlap_original_vs_suspect)
+        plt.title("Overlap of Original and Suspect")
+        #plt.ylim(0, 1)
     
-    diff_original_vs_suspect_idx = np.nonzero(outputs_original != outputs_suspect)[0]
-    print("Class predictions that differ for the original and the suspect model:")
-    print(f"  {frac_str(len(diff_original_vs_suspect_idx), len(outputs_original))}")
-    
-    diff_original_vs_correct_idx = np.nonzero(outputs_original != correct_outputs)[0]
-    diff_original_vs_suspect_idx = np.nonzero(outputs_original[diff_original_vs_correct_idx] != outputs_suspect[diff_original_vs_correct_idx])[0]
-    print("For the cases where the original model's predictions are not correct, class predictions that differ for the original and the suspect model:")
-    print(f"  {frac_str(len(diff_original_vs_suspect_idx), len(diff_original_vs_correct_idx))}")
-    
-    
-    print("\nAnalyzing logits.")
-    
-    diff_original_vs_suspect = logits_distance(logits_original, logits_suspect)
-    print("Logit distances between the original and suspect model:")
-    print_histogram(diff_original_vs_suspect, range=(0, 2))
-    
-    diff_original_vs_suspect = logits_distance(logits_original[diff_original_vs_correct_idx], logits_suspect[diff_original_vs_correct_idx])
-    print("For the cases where the original model's predictions are not correct, logit distances between the original and suspect model:")
-    print_histogram(diff_original_vs_suspect, range=(0, 2))
-    
-    
-    print("\nAnalyzing traces.")
-    
-    print("Calculating overlap between original and suspect model:")
-    overlap_original_vs_suspect = overlap_between_traces(traces_original, traces_suspect)
-    
-    print("Overlap area original vs suspect:")
-    print_histogram(overlap_original_vs_suspect, range=(0, 1))
-    
-    plt.figure()
-    plt.plot(overlap_original_vs_suspect)
-    plt.title("Overlap of Original and Suspect")
-    #plt.ylim(0, 1)
-    
-    print("T-test between original and suspect model:")
-    ttest_original_vs_suspect = ttest(traces_original, traces_suspect)
-    
-    plt.figure()
-    plt.plot(ttest_original_vs_suspect)
-    plt.title("T-test between Original and Suspect")
-    
-    print("Correlation between original and suspect model:")
+        ttest_original_vs_suspect = ttest(traces_original, traces_suspect)
+        
+        plt.figure()
+        plt.plot(ttest_original_vs_suspect)
+        plt.title("T-test between Original and Suspect")
+
+    if verbose:
+        print("Correlation between original and suspect model:")
+
     corr_original_vs_suspect = correlation(traces_original, traces_suspect)
     
-    plt.figure()
-    plt.plot(corr_original_vs_suspect)
-    plt.title("Correlation between original and suspect model")
-    plt.ylim(-1, 1)
+    if plot:
+        plt.figure()
+        plt.plot(corr_original_vs_suspect)
+        plt.title("Correlation between original and suspect model")
+        plt.ylim(-1, 1)
+
+    flat_corr = flat_correlation(traces_original, traces_suspect)
+
+    if verbose:
+        print("Correlation between flattened versions of original and suspect model:", flat_corr)
     
-    print("Correlation between flattened versions of original and suspect model:", flat_correlation(traces_original, traces_suspect))
-    
-    
-    print("\nUsing only inputs where the original model produce incorrect predictions, together with significant trace points.")
-    a = traces_original[diff_original_vs_correct_idx]
-    b = traces_suspect[diff_original_vs_correct_idx]
-    
-    print("Calculating overlap between original and suspect model:")
-    overlap_original_vs_suspect = overlap_between_traces(a, b)
-    
-    print("Overlap area original vs suspect:")
-    print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+    if correct_outputs is not None:
+        if verbose:
+            print("\nUsing only inputs where the original model produce incorrect predictions, together with significant trace points.")
+        a = traces_original[diff_original_vs_correct_idx]
+        b = traces_suspect[diff_original_vs_correct_idx]
+
+        if verbose:
+            print("Calculating overlap between original and suspect model:")
+
+        overlap_original_vs_suspect = overlap_between_traces(a, b, method=overlap_method, verbose=verbose)
+        results["trace_overlap_when_orig_wrong"] = np.average(overlap_original_vs_suspect)
+
+        if verbose:
+            print("Overlap area original vs suspect:")
+            print_histogram(overlap_original_vs_suspect, range=(0, 1))
+
+    return results
+
+
+
+def decimate_traces(traces, decimation_factor=2, n_decimation=3):
+    for i in range(n_decimation):
+        traces = sig.decimate(traces, decimation_factor)
+    return traces
     
 
 #
@@ -313,7 +396,27 @@ if __name__ == '__main__':
     print("suspect_prefix:", suspect_prefix)
     print("correct_outputs_file:", correct_outputs_file)
 
-    compare_models(original_prefix, suspect_prefix, correct_outputs_file)
+    outputs_original = np.load(original_prefix + "_outputs.npy")
+    outputs_suspect = np.load(suspect_prefix + "_outputs.npy")
+    correct_outputs = np.load(correct_outputs_file)
+
+    traces_original = np.load(original_prefix + "_traces.npy")
+    traces_suspect = np.load(suspect_prefix + "_traces.npy")
+
+    decimation_factor = 2
+    n_decimation = 3
+    print("decimation:", decimation_factor ** n_decimation)
+
+    traces_original = decimate_traces(traces_original, decimation_factor, n_decimation)
+    traces_suspect = decimate_traces(traces_suspect, decimation_factor, n_decimation)    
+
+    results = compare_model_outputs(outputs_original, outputs_suspect, correct_outputs, verbose=True)
+    results_2 = compare_traces(outputs_original, outputs_suspect, correct_outputs, traces_original, traces_suspect, verbose=True, plot=True)
+
+    results = {**results, **results_2}
+
+    print("\nSummary:")
+    print(results)
     
     plt.show()
     
